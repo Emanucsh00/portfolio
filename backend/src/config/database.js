2536bd3c1e1e65a118f63c8cbee0e52b6ed28932
PC1\Castro <emanucaz10111@gmail.com>
2026-05-15 23:23:51 -0600

    CASTRO: cambiando BD a postgres

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,19 +4,25 @@ import { Sequelize } from 'sequelize';
 dotenv.config();
 
 const {
+  NODE_ENV,
   DATABASE_URL,
-  DB_DIALECT = 'mariadb',
+  DB_DIALECT = 'postgres',
   DB_HOST = '127.0.0.1',
-  DB_PORT = '3306',
+  DB_PORT = '5432',
   DB_NAME = 'portfolio_db',
   DB_USER = 'root',
   DB_PASSWORD = ''
 } = process.env;
 
+const sslOptions = NODE_ENV === 'production'
+  ? { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } }
+  : {};
+
 export const sequelize = DATABASE_URL
   ? new Sequelize(DATABASE_URL, {
-      dialect: DB_DIALECT,
-      logging: false
+      dialect: DB_DIALECT || 'postgres',
+      logging: false,
+      ...sslOptions
     })
   : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
       host: DB_HOST,
